87d7385b5aa3c71b9b34059eeb178ff485467b2e
Maciej Matuszewski <maciej.matuszewski@netguru.pl>
2018-02-12 19:45:29 +0100

    Initial Commit

---
+++ Geodezja WTC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Geodezja WTC
+//
+//  Created by Maciej Matuszewski on 12.02.2018.
+//  Copyright © 2018 Maciej Matuszewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
